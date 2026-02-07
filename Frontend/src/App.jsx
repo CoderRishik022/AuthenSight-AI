51fb21f9002391565448e6e5fb4b38531090c4a7
groxyash <yashgoyal.ec@gmail.com>
2026-02-07 16:43:22 +0530

    basic backend setup and model definition

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home.js'
+import Profiles from './pages/Profiles.js'
+import Reports from './pages/Reports.js'
+
 import './App.css'
 
 function App() {
   
-
   return (
-    <></>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/profiles" element={<Profiles/>} />
+      <Route path="/reports" element={<Reports/>} />
+    </Routes>
   )
 }
 
