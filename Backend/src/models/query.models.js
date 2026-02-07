5fc6008a680f8f3d4a4a35052c47685157083f38
Aman0728 <mcool3807@gmail.com>
2026-02-07 16:34:10 +0530

    basic backend setup and user and query model created

---
+++ Backend/src/models/query.models.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+const querySchema = new Schema(
+    {
+        owner: {
+            type: mongoose.Types.ObjectId,
+            ref: "User"
+        },
+        queryObject: {
+            type: String,
+            required: true
+        },
+        ansPerc: {
+            type: String,
+            required: true
+        },
+        ansClaim: {
+            type: Boolean,
+            required: true
+        }
+    }
+,{timestamps: true})
+
+export const Query = mongoose.model("Query", querySchema)
